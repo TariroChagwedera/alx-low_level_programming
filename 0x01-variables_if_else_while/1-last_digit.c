bf3ed9afac3918d951fb759c31c77f2f88117fe0
TariroChagwedera <mhemberetariro@gmail.com>
2023-04-21 16:01:06 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,9 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point
- * Description: 'To get the last digit of a number'
- * Return: always 0
+ *main - Entry point
+ *Description: 'To get the last digit of a number'
+ *Return: always 0
  */
 int main(void)
 {
